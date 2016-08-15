bb7478aa09b6cddf189d83794aeab04ffc8cd6d6
Riveascore <ChrisAga.dev@gmail.com>
2016-08-15 01:11:19 -0500

    Setup fetchPost action creator

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,4 +1,4 @@
-import { FETCH_POSTS } from '../actions/index';
+import { FETCH_POSTS, FETCH_POST } from '../actions/index';
 
 const INITIAL_STATE = {
   all: [],
@@ -14,6 +14,11 @@ export default function(state = INITIAL_STATE, action) {
       ...state,
       all: action.payload.data
     };
+    case FETCH_POST:
+      return {
+        ...state,
+        post: action.payload.data
+      };
   default:
     return state;
   }
