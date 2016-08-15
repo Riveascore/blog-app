71ce46fac30c1fc594a48414ff1c51d09c29337c
Riveascore <ChrisAga.dev@gmail.com>
2016-08-14 19:39:43 -0500

    Call fetch posts on componentWillMount, refactor

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
 
 class PostsIndex extends Component {
   componentWillMount() {
-    console.log('good time to call action creator to fetch posts');
+    this.props.fetchPosts();
   }
 
   render() {
@@ -14,4 +16,4 @@ class PostsIndex extends Component {
   }
 }
 
-export default PostsIndex;
+export default connect(null, { fetchPosts })(PostsIndex);
