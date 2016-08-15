929b6ef861a6f49d9ac2f816468ae44f57adeb03
Riveascore <ChrisAga.dev@gmail.com>
2016-08-14 23:44:09 -0500

    Finish form validations and add cancel Link

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsNew extends Component {
   render() {
@@ -20,31 +21,61 @@ class PostsNew extends Component {
         <h3>
           Create a New Post
         </h3>
-        <div className="form-group">
+        <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
           <label>
             Title
           </label>
           <input type="text" className="form-control" {...title} />
+          <div className="form-control-feedback">
+            {title.touched ? title.error : ""}
+          </div>
         </div>
-        <div className="form-group">
+
+        <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
           <label>
             Categories
           </label>
           <input type="text" className="form-control"  {...categories} />
+          <div className="form-control-feedback">
+            {categories.touched ? categories.error : ""}
+          </div>
         </div>
-        <div className="form-group">
+
+        <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
           <label>
             Content
           </label>
           <textarea className="form-control" {...content} />
+          <div className="form-control-feedback">
+            {content.touched ? content.error : ""}
+          </div>
         </div>
 
         <button type="submit" className="btn btn-primary">
           Submit
         </button>
+        <Link to="/" className="btn btn-default">
+          Cancel
+        </Link>
       </form>
     );
   }
+}
+
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = "Title can't be blank";
+  }
+  if (!values.categories) {
+    errors.categories = "Enter Categories";
+  }
+  if (!values.content) {
+    errors.content = "Enter Some Content";
+  }
+
+  return errors;
 }
 
 // When User types something in... reduxForm records it on application state
@@ -79,5 +110,6 @@ export default reduxForm({
     'title',
     'categories',
     'content'
-  ]
+  ],
+  validate
 }, null, { createPost })(PostsNew);
